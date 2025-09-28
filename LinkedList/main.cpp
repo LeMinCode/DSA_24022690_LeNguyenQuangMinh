a4124ddaafb78fbab118c8d03553788ca36b01a3
LeMinhCode <24022690@vnu.edu.vn>
2025-09-28 15:21:42 +0700

    DSA

---
+++ LinkedList/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "LinkedList.h"
+using namespace std;
+
+int main() {
+    LinkedList L;
+
+    L.AddLast(1);
+    L.AddLast(2);
+    L.AddLast(3);
+    L.printNor(); //1 2 3
+
+    L.AddFirst(0);
+    L.printNor(); //0 1 2 3
+
+    L.InsertP(2, 88);
+    L.printNor(); //0 1 2 88 2 3
+
+    L.DeleteFirst();
+    L.printNor(); //1 2 88 3
+
+    L.DeleteLast();
+    L.printNor(); //1 88 2
+
+    L.DeletePos(2);
+    L.printNor(); //1 88
+
+    L.printReverse(L.head); //88 1
+    return 0;
+}
+
